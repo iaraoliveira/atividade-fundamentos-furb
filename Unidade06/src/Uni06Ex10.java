bdb14157f4c75f22eef572315d5990bf00d83ef4
iaraoliveira <iara99oliveira@gmail.com>
2023-08-24 21:28:12 -0300

    coisas novas

---
+++ Unidade06/src/Uni06Ex10.java
@@ -0,0 +1,17 @@
+public class Uni06Ex10 {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 0;
+        b = 15;
+        c = 0;
+        while (a <= b) {
+               a = a + 3;
+               b = b - a;
+               c = b - a;
+        }
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+
+    }
+}
